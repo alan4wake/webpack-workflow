4cc879403a37edf0f91d3183c54678c9b099921c
liuming <liuming@autohome.com.cn>
2016-04-07 15:33:53 +0800

    version v1.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
+var bower_dir = __dirname + '/bower_component';
 module.exports = {
   entry: ['./app/main.js'],
+  resolve: {
+    alias: {
+      'react': bower_dir + '/react/react.min.js',
+      'react-dom': bower_dir + '/react/react-dom.min.js'
+    }
+  },
   output: {
     path: './build',
     filename: 'bundle.js'
   },
   module: {
+    noParse: [bower_dir + '/react/react.min.js', bower_dir + '/react/react-dom.min.js'],
     loaders: [
       {
         test: /\.js$/,
